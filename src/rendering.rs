336517236dbe8ef3fc01cc7834aa444b57d446ac
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-08-16 21:36:24 +0100

    finish basic renderer

--- src/rendering.rs
+++ src/rendering.rs
@@ -13,7 +13,7 @@ pub struct Renderer {
 
 impl Renderer {
     pub async fn new(grass_tex: &str, structures: Vec<Structure>) -> Self {
-        let scale = 0.01;
+        let scale = 1.0;
         let grass: Texture2D = load_texture(grass_tex).await.unwrap();
         grass.set_filter(FilterMode::Nearest);
 
@@ -42,27 +42,31 @@ impl Renderer {
     }
 
     pub fn draw(&self) {
-        set_camera(&self.rt_camera);
-
-        for i in -0..1 {
-            for j in -0..1 {
-                draw_texture_ex(
-                    &self.grass,
-                    self.render_target.texture.width() / 2.0 + (i - 1 - j) as f32 * self.tile_dim.x,
-                    self.render_target.texture.height() / 2.0 + (j - 1 + i) as f32 * self.tile_dim.y,
-                    WHITE,
-                    DrawTextureParams {
-                        dest_size: Some(Vec2::new(self.grass.width(), self.grass.height())),
-                        ..Default::default()
-                    },
-                );
-            }
-        }
         
-        set_default_camera();
+        // Number of tiles to draw in each direction from the centre on the render target
+        const TO_NEXT_TEX: i32 = 1;
+        
+        for i in -30..31 {
+            for j in -30..31 {
+                set_camera(&self.rt_camera);
 
-        for i in -15..16 {
-            for j in -15..16 {
+                for k in -TO_NEXT_TEX..TO_NEXT_TEX+1 {
+                    for l in -TO_NEXT_TEX..TO_NEXT_TEX+1 {
+                        draw_texture_ex(
+                            &self.grass,
+                            self.render_target.texture.width() / 2.0 + (k - 1 - l) as f32 * self.tile_dim.x / (TO_NEXT_TEX * 2 + 1) as f32,
+                            self.render_target.texture.height() / 2.0 + (l - 1 + k) as f32 * self.tile_dim.y / (TO_NEXT_TEX * 2 + 1) as f32,
+                            WHITE,
+                            DrawTextureParams {
+                                dest_size: Some(Vec2::new(self.grass.width() / (TO_NEXT_TEX * 2 + 1) as f32, self.grass.height() / (TO_NEXT_TEX * 2 + 1) as f32)),
+                                ..Default::default()
+                            },
+                        );
+                    }
+                }
+                
+                set_default_camera();
+
                 draw_texture_ex(
                     &self.render_target.texture,
                     screen_width() / 2.0 - 1.0 * self.render_target.texture.width() / 2.0 * self.scale + (i - j) as f32 * self.tile_dim.x * self.scale + self.scroll.x,
@@ -76,23 +80,8 @@ impl Renderer {
             }
         }
 
-        // for i in -15..15 {
-        //     for j in -15..15 {
-        //         draw_texture_ex(
-        //             &texture,
-        //             screen_width() / 2.0 + (i - 1 - j) as f32 * tile_dim.x * scale + scroll.x,
-        //             screen_height() / 2.0 + (j - 1 + i) as f32 * tile_dim.y * scale + scroll.y,
-        //             WHITE,
-        //             DrawTextureParams {
-        //                 dest_size: Some(Vec2::new(texture.width() * scale, texture.height() * scale)),
-        //                 ..Default::default()
-        //             },
-        //         );
-        //     }
-        // }
-
         for structure in &self.structures {
-            structure.draw(vec2(0.0, 0.0), &self.scale, &self.tile_dim, &self.scroll);
+            structure.draw(vec2(0.0, 0.0), &(self.scale / (TO_NEXT_TEX * 2 + 1) as f32), &self.tile_dim, &self.scroll);
         }
     }
 }
