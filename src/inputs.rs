89a79f2fcbea204e29c0a057665048cc3c834003
Nathan Ilsley <nathan.ilsley@gmail.com>
2025-08-18 19:32:49 +0100

    Refactor input handling and enhance world rendering logic

--- src/inputs.rs
+++ src/inputs.rs
@@ -8,14 +8,28 @@ pub struct InputHandler {
 impl InputHandler {
     pub fn new() -> Self {
         Self {
-            scroll_speed: 50.0,
+            scroll_speed: 100.0,
         }
     }
 
     pub fn take_input(&self, world_drawer: &mut WorldDrawer) {
         if is_key_down(KeyCode::Left) {
+            world_drawer.change_scroll(get_frame_time() * self.scroll_speed * vec2(1.0, 0.0));
+        }
+        if is_key_down(KeyCode::Right) {
             world_drawer.change_scroll(get_frame_time() * self.scroll_speed * vec2(-1.0, 0.0));
-            // println!("Left key pressed");
+        }
+        if is_key_down(KeyCode::Up) {
+            world_drawer.change_scroll(get_frame_time() * self.scroll_speed * vec2(0.0, 1.0));
+        }
+        if is_key_down(KeyCode::Down) {
+            world_drawer.change_scroll(get_frame_time() * self.scroll_speed * vec2(0.0, -1.0));
+        }
+        if is_key_pressed(KeyCode::Equal) {
+            world_drawer.change_zoom(1);
+        }
+        if is_key_pressed(KeyCode::Minus) {
+            world_drawer.change_zoom(-1);
         }
     }
 }
--- src/world_drawer.rs
+++ src/world_drawer.rs
@@ -1,18 +1,111 @@
 use macroquad::prelude::*;
 use crate::structures::Structure;
+use crate::tiles::Tile;
 use crate::tiles::TileManager;
+
+pub struct Chunk {
+    chunk_pos: Vec2,
+    to_next_chunk: i32,
+    tile_dim: Vec2,
+    live_chunk: bool,
+    built: bool,
+    tiles: Vec<Vec<Tile>>,
+    render_target: RenderTarget,
+    rt_camera: Camera2D,
+}
+
+impl Chunk {
+    pub fn new(chunk_pos: Vec2, to_next_chunk: i32, tile_dim: Vec2, live_chunk: bool) -> Self {
+        let render_target = render_target(256, 256);
+        render_target.texture.set_filter(FilterMode::Nearest);
+        let cam_render_target = render_target.clone();
+
+        Self {
+            chunk_pos,
+            to_next_chunk,
+            tile_dim,
+            live_chunk,
+            built: false,
+            tiles: Vec::new(),
+            render_target,
+            rt_camera: Camera2D {
+                render_target: Some(cam_render_target),
+                ..Camera2D::from_display_rect(Rect::new(0.0, 0.0, 256.0, 256.0))
+            },
+        }
+    }
+
+    pub fn is_built(&self) -> bool {
+        self.built
+    }
+
+    pub fn set_live_chunk(&mut self, live_chunk: bool) {
+        self.live_chunk = live_chunk;
+    }
+
+    pub fn is_live_chunk(&self) -> bool {
+        self.live_chunk
+    }
+
+    pub fn build(&mut self, tile_manager: &TileManager) {
+        let chunk_dist = self.to_next_chunk as i32 * 2 + 1;
+        let chunk_centre = Vec2::new(self.chunk_pos.x as f32 * chunk_dist as f32, self.chunk_pos.y as f32 * chunk_dist as f32);
+        
+        let mut tiles = Vec::new();
+        for i in (-self.to_next_chunk as i32 + chunk_centre.x as i32)..(self.to_next_chunk as i32 + 1 + chunk_centre.x as i32) {
+            tiles.push(Vec::new());
+            for j in (-self.to_next_chunk as i32 + chunk_centre.y as i32)..(self.to_next_chunk + 1 + chunk_centre.y as i32) {
+                tiles[(i + self.to_next_chunk - chunk_centre.x as i32) as usize].push(tile_manager.get_tile(i as isize, j as isize).unwrap_or(&Tile::Grass()).clone());
+            }
+        }
+
+        self.tiles = tiles;
+        self.built = true;
+    }
+
+    pub fn draw_to_render_target(&mut self, grass: &Texture2D) {
+        set_camera(&self.rt_camera);
+
+        for k in -self.to_next_chunk..self.to_next_chunk+1 {
+            for l in -self.to_next_chunk..self.to_next_chunk+1 {
+                draw_texture_ex(
+                    grass,
+                    self.render_target.texture.width() / 2.0 + (k - 1 - l) as f32 * self.tile_dim.x / (self.to_next_chunk * 2 + 1) as f32,
+                    self.render_target.texture.height() / 2.0 + (l - 1 + k) as f32 * self.tile_dim.y / (self.to_next_chunk * 2 + 1) as f32,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(Vec2::new(grass.width() / (self.to_next_chunk * 2 + 1) as f32, grass.height() / (self.to_next_chunk * 2 + 1) as f32)),
+                        ..Default::default()
+                    },
+                );
+            }
+        }
+    }
+
+    pub fn draw_to_screen(&self, scale: f32, scroll_vector: &Vec2) {
+        draw_texture_ex(
+            &self.render_target.texture,
+            screen_width() / 2.0 - 1.0 * self.render_target.texture.width() / 2.0 * scale + (self.chunk_pos.x - self.chunk_pos.y) * self.tile_dim.x * scale + scroll_vector.x,
+            screen_height() / 2.0 - 1.0 * self.render_target.texture.height() / 2.0 * scale + (self.chunk_pos.y + self.chunk_pos.x) * self.tile_dim.y * scale + scroll_vector.y,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(self.render_target.texture.width() * scale, self.render_target.texture.height() * scale)),
+                ..Default::default()
+            },
+        );
+    }
+}
+
+const CHUNKS_FROM_CENTRE: usize = 5;
 
 pub struct WorldDrawer {
     scale: f32,
     // Number of tiles to draw in each direction from the centre on the render target
-    to_text_chunk: i32,
+    to_next_chunk: i32,
     grass: Texture2D,
     tile_dim: Vec2,
 
-    render_chunk: RenderTarget,
-    rc_camera: Camera2D,
-    all_grass_chunk: RenderTarget,
-    agc_camera: Camera2D,
+    chunks: Vec<Vec<Chunk>>,
 
     scroll_vector: Vec2,
     structures: Vec<Structure>,
@@ -20,43 +113,31 @@ pub struct WorldDrawer {
 
 impl WorldDrawer {
     pub async fn new(grass_tex: &str, structures: Vec<Structure>) -> Self {
-        let scale = 1.0;
         let grass: Texture2D = load_texture(grass_tex).await.unwrap();
         grass.set_filter(FilterMode::Nearest);
+
+        let to_next_chunk = 10;
 
         // x and y dimensions of the base game tile measured from the centre of the tile to each corner
         let tile_dim = vec2(grass.width() / 2.0, grass.height() / 2.0);
 
-        let render_chunk = render_target(256, 256);
-        render_chunk.texture.set_filter(FilterMode::Nearest);
-
-        let rc_camera = Camera2D {
-            render_target: Some(render_chunk.clone()),
-            ..Camera2D::from_display_rect(Rect::new(0.0, 0.0, 256.0, 256.0))
-        };
-
-        let all_grass_chunk = render_target(256, 256);
-        all_grass_chunk.texture.set_filter(FilterMode::Nearest);
-
-        let agc_camera = Camera2D {
-            render_target: Some(all_grass_chunk.clone()),
-            ..Camera2D::from_display_rect(Rect::new(0.0, 0.0, 256.0, 256.0))
-        };
-
-        let scroll = vec2(0.0, 0.0);
+        let mut chunks = Vec::new();
+        for i in -(CHUNKS_FROM_CENTRE as i32)..CHUNKS_FROM_CENTRE as i32 + 1 {
+            chunks.push(Vec::new());
+            for j in -(CHUNKS_FROM_CENTRE as i32)..CHUNKS_FROM_CENTRE as i32 + 1 {
+                chunks[(i + CHUNKS_FROM_CENTRE as i32) as usize].push(Chunk::new(vec2(i as f32, j as f32), to_next_chunk, tile_dim, false)); 
+            }
+        }
 
         Self {
-            scale,
-            to_text_chunk: 5,
-            tile_dim,
+            scale: 1.0,
+            to_next_chunk,
             grass,
+            tile_dim,
 
-            render_chunk,
-            rc_camera,
-            all_grass_chunk,
-            agc_camera,
+            chunks,            
 
-            scroll_vector: scroll,
+            scroll_vector: vec2(0.0, 0.0),
             structures,
         }
     }
@@ -65,51 +146,34 @@ impl WorldDrawer {
         self.scroll_vector += delta;
     }
 
-    fn assemble_render_chunk(&self) {
-        set_camera(&self.rc_camera);
-
-        for k in -self.to_text_chunk..self.to_text_chunk+1 {
-            for l in -self.to_text_chunk..self.to_text_chunk+1 {
-                draw_texture_ex(
-                    &self.grass,
-                    self.render_chunk.texture.width() / 2.0 + (k - 1 - l) as f32 * self.tile_dim.x / (self.to_text_chunk * 2 + 1) as f32,
-                    self.render_chunk.texture.height() / 2.0 + (l - 1 + k) as f32 * self.tile_dim.y / (self.to_text_chunk * 2 + 1) as f32,
-                    WHITE,
-                    DrawTextureParams {
-                        dest_size: Some(Vec2::new(self.grass.width() / (self.to_text_chunk * 2 + 1) as f32, self.grass.height() / (self.to_text_chunk * 2 + 1) as f32)),
-                        ..Default::default()
-                    },
-                );
-            }
+    pub fn change_zoom(&mut self, delta: i32) {
+        if self.to_next_chunk - delta >= 0 && self.to_next_chunk - delta <= 10 {
+            self.to_next_chunk -= delta;
         }
     }
 
-    fn draw_render_chunk(&self, grid_x: i32, grid_y: i32) {
+    pub fn draw(&mut self, tile_manager: &TileManager) {
+        for i in 0..self.chunks.len() {
+            for j in 0..self.chunks[i].len() {
+                if !self.chunks[i][j].is_built() {
+                    self.chunks[i][j].build(tile_manager);
+                    self.chunks[i][j].draw_to_render_target(&self.grass);
+                }
+            }
+        }
+
         set_default_camera();
 
-        draw_texture_ex(
-            &self.render_chunk.texture,
-            screen_width() / 2.0 - 1.0 * self.render_chunk.texture.width() / 2.0 * self.scale + (grid_x - grid_y) as f32 * self.tile_dim.x * self.scale + self.scroll_vector.x,
-            screen_height() / 2.0 - 1.0 * self.render_chunk.texture.height() / 2.0 * self.scale + (grid_y + grid_x) as f32 * self.tile_dim.y * self.scale + self.scroll_vector.y,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(Vec2::new(self.render_chunk.texture.width() * self.scale, self.render_chunk.texture.height() * self.scale)),
-                ..Default::default()
-            },
-        );
-    }
-
-    pub fn draw(&self, tile_manager: &TileManager) {
-        for i in -5..6 {
-            for j in -5..6 {
-                self.assemble_render_chunk();
-
-                self.draw_render_chunk(i, j);
+        for i in 0..self.chunks.len() {
+            for j in 0..self.chunks[i].len() {
+                self.chunks[i][j].draw_to_screen(self.scale, &self.scroll_vector);
             }
         }
 
         for structure in &self.structures {
-            structure.draw(vec2(0.0, 0.0), &(self.scale / (self.to_text_chunk * 2 + 1) as f32), &self.tile_dim, &self.scroll_vector);
+            structure.draw(vec2(0.0, 0.0), &(self.scale / (self.to_next_chunk * 2 + 1) as f32), &self.tile_dim, &self.scroll_vector);
+            structure.draw(vec2(115.0, 115.0), &(self.scale / (self.to_next_chunk * 2 + 1) as f32), &self.tile_dim, &self.scroll_vector);
+            structure.draw(vec2(10.0, 10.0), &(self.scale / (self.to_next_chunk * 2 + 1) as f32), &self.tile_dim, &self.scroll_vector);
         }
     }
 }
